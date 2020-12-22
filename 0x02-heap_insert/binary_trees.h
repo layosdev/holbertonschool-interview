86af27b648ecc5c71f90ecce39690a6d59d07d0f
Felipe Layos <jfelipelayos@gmail.com>
2020-12-22 11:54:41 -0500

    Prototypes added to header

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -27,4 +27,8 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+size_t depth_check(const binary_tree_t *tree);
+int leaf_check(const binary_tree_t *node);
+binary_tree_t *insert_node(binary_tree_t *tree, size_t level, int value);
+size_t height_check(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
