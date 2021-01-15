df3ecb462233b8bcaefddb6efa9ea08bfea3c3b6
Felipe Layos <jfelipelayos@gmail.com>
2021-01-14 20:19:23 -0500

    Header file

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print(int grid[3][3]);
+void sum(int grid1[3][3], int grid2[3][3]);
+void reset(int grid[3][3]);
+#endif
