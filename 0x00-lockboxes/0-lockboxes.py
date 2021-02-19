c9407f01796c46740d8e3cd29275c04913de544d
Felipe Layos <jfelipelayos@gmail.com>
2021-02-19 11:40:48 -0500

    Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
+"""Lockboxes"""
+
+
 def canUnlockAll(boxes):
-    seen = [False] * len(boxes)
-    seen[0] = True
-    stack = [0]
-    while stack:
-        node = stack.pop()
-        for nei in boxes[node]:
-            if not seen[nei]:
-                seen[nei] = True
-                stack.append(nei)
-    return all(seen)
+    """determines if all the boxes can be opened."""
+
+    box_status = False
+    keys = [0]
+    arr_index = []
+    arr_size = len(boxes)
+
+    for i in range(arr_size):
+        arr_index.append(i)
+
+    while len(arr_index):
+        arr_aux = []
+        for key in keys:
+            if key in arr_index:
+                box_status = True
+                for box in boxes[key]:
+                    if box in arr_index and box not in keys:
+                        arr_aux.append(box)
+                arr_index.remove(key)
+        keys.extend(arr_aux)
+
+        if not box_status:
+            return False
+        box_status = False
+    return True
