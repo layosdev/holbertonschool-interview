de1219d4c8321251159a45b53818627cf487fd1d
Felipe Layos <jfelipelayos@gmail.com>
2021-03-02 12:49:49 -0500

    2D Menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,43 @@
+#include "menger.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * menger - draws a 2D Menger Sponge
+ * @level: depth
+ */
+void menger(int level)
+{
+	int i, j, k, m;
+	int sw = 1, depth;
+
+	depth = pow(3, level);
+
+	for (i = 1; i <= depth; i++)
+	{
+		for (j = 1; j <= depth; j++)
+		{
+			m = i - 1;
+			k = j - 1;
+			while (sw)
+			{
+				if (k == 0 || m == 0)
+				{
+					printf("#");
+					break;
+				}
+				else if (k % 3 == 1 && m % 3 == 1)
+				{
+					printf(" ");
+					break;
+				} else
+				{
+					k /= 3;
+					m /= 3;
+				}
+			}
+		}
+		printf("\n");
+	}
+}
