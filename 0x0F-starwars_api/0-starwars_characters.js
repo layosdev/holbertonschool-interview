098f14466f135c65095a62a11b472dffd9df9588
Felipe Layos <jfelipelayos@gmail.com>
2021-04-08 12:05:51 -0500

    Star Wars Characters - Fix semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const URL = "https://swapi-api.hbtn.io/api/films/" + args[0];
-const request = require("request");
+const URL = 'https://swapi-api.hbtn.io/api/films/' + args[0];
+const request = require('request');
 
-let getCharacterName = (link) => {
-    const characterName = new Promise((resolve, reject) => {
-        request(link, (error, response, body) => {
-            if (error) console.error("error:", error);
-            resolve(JSON.parse(body).name);
-        });
+const getCharacterName = (link) => {
+  const characterName = new Promise((resolve, reject) => {
+    request(link, (error, response, body) => {
+      if (error) console.error('error:', error);
+      resolve(JSON.parse(body).name);
     });
-    return characterName;
+  });
+  return characterName;
 };
 
 request(URL, async (error, response, body) => {
-    if (error) console.error("error:", error);
-    let characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-        let characterName = await getCharacterName(characters[i]);
-        console.log(characterName);
-    }
+  if (error) console.error('error:', error);
+  const characters = JSON.parse(body).characters;
+  for (let i = 0; i < characters.length; i++) {
+    const characterName = await getCharacterName(characters[i]);
+    console.log(characterName);
+  }
 });
